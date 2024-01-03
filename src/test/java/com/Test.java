8189649f0d56dbd2e81cc949cb8b71c72f4f3989
ashwini-chaudhari <chaudhariashwini112@gmail.com>
2024-01-03 17:23:39 +0530

    3 Jan 2024

--- src/test/java/com/Test.java
+++ src/test/java/com/Test.java
@@ -3,7 +3,9 @@ package com;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test 1");	
+		System.out.println("Test 1");
+		
+		System.out.println("Test 2");
 		}
 
 }
